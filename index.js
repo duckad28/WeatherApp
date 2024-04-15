3446359f75b6122b4c209c08c0373d3c6a1b4d69
duckad28 <153713020+duckad28@users.noreply.github.com>
2024-04-15 07:50:44 +0700

    Add new project WeatherApp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import App from './navigation/app'
+import {MainScreen} from './screens';
+AppRegistry.registerComponent(appName, () => () => <App />);
